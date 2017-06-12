708aba65d8e86dc7be4f0784dab2b367cfd7ca01
Touko Hallasmaa <touko.hallasmaa@outlook.com>
2017-06-12 14:50:12 +0300

    update latest migration to add function for renewing token with refresh token

--- migrations/20170612120300_add_table_for_refresh_tokens_and_client_ids.js
+++ migrations/20170612120300_add_table_for_refresh_tokens_and_client_ids.js
@@ -9,6 +9,32 @@ exports.up = knex => knex.schema.withSchema('auth').createTable('client', table 
   table.integer('account')
     .references('id').inTable('auth.login')
     .onDelete('CASCADE');
-});
+})
+  .then(() => knex.raw(`
+    CREATE OR REPLACE FUNCTION auth.authenticate_with_refresh_token(client_id TEXT, refresh_token TEXT) RETURNS auth.jwt_claim
+    AS $$
+      DECLARE result auth.jwt_claim;
+      BEGIN
+        SELECT
+          role,
+          login.id AS account_id,
+          extract(epoch from now())::integer + 3600 as exp
+        FROM auth.client
+        JOIN auth.login ON
+          login.id = client.account
+        WHERE
+          client.id = authenticate_with_refresh_token.client_id AND
+          client.refresh_token = authenticate_with_refresh_token.token
+        INTO result;
+        IF result IS null THEN
+          RAISE invalid_password USING MESSAGE = 'invalid refresh token';
+        END IF;
+        RETURN result;
+      END;
+    $$ LANGUAGE plpgsql
+  `))
+  .then('GRANT EXECUTE ON FUNCTION auth.authenticate_with_refresh_token(TEXT, TEXT) TO guest');
 
-exports.down = knex => knex.schema.withSchema('auth').dropTable('client');
+exports.down = knex =>
+  knex.raw('DROP FUNCTION auth.authenticate_with_refresh_token(TEXT, TEXT)')
+    .then(() => knex.schema.withSchema('auth').dropTable('client'));
