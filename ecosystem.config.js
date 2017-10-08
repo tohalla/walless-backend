18311ed219c6ece066ae92baff8e543998d69daa
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2017-10-08 22:55:38 +0300

    update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'walless-backend',
       script: './dist/index.js',
       watch: true,
+      ignore_watch: ['schema.json'],
       instance_var: 'INSTANCE_ID',
       env: {
         'PORT': 8080,
